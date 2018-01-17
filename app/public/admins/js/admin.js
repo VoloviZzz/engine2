d4f885dcfb732b1c0f783693aa0c6b2b2c7f6f83
Zuarel <zuarel@icloud.com>
2018-01-17 20:54:00 +0500

    merge

--- app/public/admins/js/admin.js
+++ app/public/admins/js/admin.js
@@ -1,59 +1,59 @@
-function Fragments() {}
+function Fragments() { }
 
 Fragments.prototype.add = function (route_id) {
-    $.post("/api/fragments/add", { route_id }).done((result) => {
-        if (result.status === 'ok') return location.reload();
-        console.log(result);
-        alert(result.message)
-    })
+	$.post("/api/fragments/add", { route_id }).done((result) => {
+		if (result.status === 'ok') return location.reload();
+		console.log(result);
+		alert(result.message)
+	})
 }
 
 Fragments.prototype.changeComponent = function (fragment_id, value) {
-    
-    const target = 'component_id';
 
-    $.post('/api/fragments/upd', { value, target, fragment_id }).done((result) => {
-        if(result.status === 'ok') return location.reload();
+	const target = 'component_id';
 
-        alert(result.message);
-    }).catch((error) => {
-        alert('Произошла ошибка. Попробуйте позже');
-    })
+	$.post('/api/fragments/upd', { value, target, fragment_id }).done((result) => {
+		if (result.status === 'ok') return location.reload();
+
+		alert(result.message);
+	}).catch((error) => {
+		alert('Произошла ошибка. Попробуйте позже');
+	})
 }
 
-Fragments.prototype.delete = function(fragment_id) {
-    console.log(this);
+Fragments.prototype.delete = function (fragment_id) {
+	console.log(this);
 }
 
 const fragments = new Fragments();
 
-$('.js-upd-fragment-component').on('change', function() {
-    return fragments.changeComponent(this.dataset.fragmentId, this.value);
+$('.js-upd-fragment-component').on('change', function () {
+	return fragments.changeComponent(this.dataset.fragmentId, this.value);
 })
 
-$('.js-add-fragment').on('click', function() {
-    return fragments.add(this.dataset.id);
+$('.js-add-fragment').on('click', function () {
+	return fragments.add(this.dataset.id);
 })
 
-$('.js-fragment-delete').on('click', function() {
-    return fragments.delete(this.dataset.routeId);
+$('.js-fragment-delete').on('click', function () {
+	return fragments.delete(this.dataset.routeId);
 })
 
 var link = document.querySelector(".setting-call-btn");
 var popup = document.querySelector(".setting-wrapper");
 
-link.addEventListener("click", function(event) {
-    event.preventDefault();
-    if (popup.classList.contains("setting-wrapper-show")) {
-        popup.classList.remove("setting-wrapper-show");
-    } else {
-        popup.classList.add("setting-wrapper-show");
-    }
+link.addEventListener("click", function (event) {
+	event.preventDefault();
+	if (popup.classList.contains("setting-wrapper-show")) {
+		popup.classList.remove("setting-wrapper-show");
+	} else {
+		popup.classList.add("setting-wrapper-show");
+	}
 
 });
 
-window.addEventListener("keydown", function(event) {
-    if (popup.classList.contains("setting-wrapper-show")) {
-        popup.classList.remove("setting-wrapper-show");
-    }
+window.addEventListener("keydown", function (event) {
+	if (popup.classList.contains("setting-wrapper-show")) {
+		popup.classList.remove("setting-wrapper-show");
+	}
 });
