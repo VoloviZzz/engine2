667ac9b502008b5e76d93dea8c9559b4d3f48411
BPSOTA <lolkek@gmail.com>
2018-02-13 15:40:56 +0500

    123

--- app/components/orders-view/index.js
+++ app/components/orders-view/index.js
@@ -7,6 +7,14 @@ module.exports = (app) => {
 	return async ({ locals, session, dataViews = {} }) => {
 		// logic...
 
+		const orderHash = locals.dynamicRouteAlias;
+		const [, [order]] = await Model.orders.get({ hash: orderHash });
+		const [, orderGoods] = await Model.ordersGoods.get({ order_id: order.id });
+
+		order.goods = orderGoods;
+		dataViews.order = order;
+		dataViews.session = session;
+
 		return new Promise((resolve, reject) => {
 			const template = app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
 				if (err) return resolve([err, err.toString()]);
