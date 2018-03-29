445cdf77d5afead4fc34a35f947a3e0639b1e175
Zuarel <zuarel@icloud.com>
2018-03-29 08:54:55 +0500

    redaktirovanie knopok v ckeditor

--- app/public/vendors/ckeditor/config.js
+++ app/public/vendors/ckeditor/config.js
@@ -3,9 +3,8 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	 config.filebrowserUploadUrl = '/api/images/uploadCKeditor'
+CKEDITOR.editorConfig = function (config) {
+	config.filebrowserUploadUrl = '/api/images/uploadCKeditor';
+	config.allowedContent = true;
+	config.removePlugins = 'spellchecker, about, save, newpage, print, templates, scayt, flash, pagebreak, smiley,preview,find'
 };
