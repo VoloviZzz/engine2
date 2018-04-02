1b440d97780eb99d1b7eebe3bbfa8dd566ab0331
Zuarel <zuarel@icloud.com>
2018-04-02 15:27:05 +0500

    pravki

--- app/public/admins/js/reviews-list.js
+++ app/public/admins/js/reviews-list.js
@@ -1,12 +1,26 @@
 $(document).ready(function (e) {
 	$('.js-reviews-togglePublished').on('click', reviewsTogglePublished);
+	$('.js-reviews-delete').on('click', reviewsDelete);
+
+	function reviewsDelete(e) {
+		var id = $(this).data('id');
+
+		$.post('/api/reviews/delete', { id: id }).done(function (result) {
+			if (result.status !== 'ok') {
+				console.log(result);
+				return alert(result.message);
+			}
+
+			location.reload();
+		});
+	}
 
 	function reviewsTogglePublished(event) {
-		const id = $(this).data('id');
-		const published = '1';
+		var id = $(this).data('id');
+		var published = '1';
 
 		$.post('/api/reviews/togglePublished', { published: published, id: id }).done(function (result) {
-			if(result.status !== 'ok') {
+			if (result.status !== 'ok') {
 				console.log(result);
 				return alert(result.message);
 			}
