32b5cd8abf6fce9bc8026b6d32799a9b7817f468
Zuarel <zuarel@icloud.com>
2018-04-19 17:42:40 +0500

    add callbacks functional

--- app/api/partners.js
+++ app/api/partners.js
@@ -1,3 +1,12 @@
+exports.get = (req, res, next) => {
+	const Model = req.app.Model;
+	
+	return Model.partners.get(req.body).then(([error]) => {
+		if (error) return { error, message: error.message };
+		return { status: 'ok' };
+	})
+}
+
 exports.update = (req, res, next) => {
 	const Model = req.app.Model;
 	
