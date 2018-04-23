a27d05562c4796c86418921281a52d1f87ce1847
Zuarel <zuarel@icloud.com>
2018-04-23 16:53:54 +0500

    achievements: component, patch, model

---
+++ app/models/achievments.js
@@ -0,0 +1,48 @@
+const db = require('../libs/db');
+
+
+exports.get = (arg = {}) => {
+
+	let id = arg.id ? `AND a.id = ${arg.id}` : ``;
+
+	let q = `
+			SELECT a.*, 
+				p.path as ach_path
+			FROM achievements a
+				LEFT JOIN photos p ON p.id = a.photo
+			WHERE a.id > 0
+				${id}
+		`;
+
+	return db.execQuery(q);
+}
+
+exports.add = (arg = {}) => {
+
+	let q = `
+			INSERT INTO achievements () VALUES ()
+		`;
+
+	return db.insertQuery(q);
+}
+
+exports.del = (arg = {}) => {
+	let q = `
+			DELETE FROM achievements
+			WHERE id = ${arg.id}
+		`;
+
+	return db.execQuery(q);
+}
+
+exports.upd = (arg = {}) => {
+	let q = `
+			UPDATE achievements
+			SET
+				${arg.target} = '${arg.value}'
+			WHERE
+				id = ${arg.id}
+		`;
+
+	return db.execQuery(q);
+}
