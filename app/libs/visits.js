bbb63ed052c4c7bfa5b4dadf32fb42f4977de26f
Zuarel <zuarel@icloud.com>
2018-05-11 20:22:05 +0500

    fix visits ip

--- app/libs/visits.js
+++ app/libs/visits.js
@@ -1,17 +1,17 @@
 const Model = require('../models');
 
 exports.createVisit = async function createVisit(req, res, next) {
-    if (!!req.session.user.visitId === false) {
+	if (!!req.session.user.visitId === false) {
 
-        let [error, visitId] = await Model.visits.add({ visitorId: req.session.user.visitorId, visitorIp: req.ip.replace(/^.*:/, '') });
+		let [error, visitId] = await Model.visits.add({ visitorId: req.session.user.visitorId, visitorIp: 'ip' in req ? req.ip.replace(/^.*:/, '') : '' });
 
-        if (error) {
-            console.log('Ошибка создания визита');
-            console.log(error);
-        }
+		if (error) {
+			console.log('Ошибка создания визита');
+			console.log(error);
+		}
 
-        req.session.user.visitId = visitId;
-    }
+		req.session.user.visitId = visitId;
+	}
 
-    return next();
+	return next();
 }
