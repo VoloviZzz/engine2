c8636022b0a102f65a9633eda79ec279590596f8
Zuarel <zuarel@icloud.com>
2018-05-21 00:56:47 +0500

    edit config ckeditor

--- app/public/vendors/ckeditor/config.js
+++ app/public/vendors/ckeditor/config.js
@@ -6,7 +6,8 @@
 CKEDITOR.editorConfig = function (config) {
 	config.filebrowserUploadUrl = '/api/images/uploadCKeditor';
 	config.allowedContent = true;
-	config.removePlugins = 'spellchecker, about, save, newpage, print, templates, scayt, flash, pagebreak, smiley,preview,find';
+	config.removePlugins = 'spellchecker,about,save,newpage,print,templates,scayt,flash,pagebreak,smiley,preview,find';
 	config.extraPlugins = 'lineheight,autogrow';
 	config.autoGrow_bottomSpace = 50;
+	config.contentsCss = '/css/imports/buttons-and-inputs.css';
 };
