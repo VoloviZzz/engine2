42f1d4101395b57ed12c827fdd70fd2b3d734125
Alex.Kruc <kililapro@gmail.com>
2018-06-06 22:28:06 +0500

    dawda

--- app/public/js/slider.init.js
+++ app/public/js/slider.init.js
@@ -96,3 +96,12 @@ $(document).ready(function () {
 		return false;
 	});
 });
+
+// Открытие закрытие мобильного слайдера
+document.addEventListener('DOMContentLoaded', function(){
+  $('.accordeon-menu__item-link').click(function(e){
+      e.preventDefault();
+      $('.accordeon-menu__item').removeClass('accordeon-menu__item--active');
+      $(this).parent(".accordeon-menu__item").addClass('accordeon-menu__item--active');
+  });
+})
