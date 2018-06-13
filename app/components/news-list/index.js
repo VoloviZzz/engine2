8ce4929759f8ba7203e9931456b9d070707f5f1e
Zuarel <zuarel@icloud.com>
2018-06-13 16:38:30 +0500

    add pagination news-list

--- app/components/news-list/index.js
+++ app/components/news-list/index.js
@@ -2,28 +2,66 @@ const path = require('path');
 const Model = require('../../models');
 
 module.exports = (app) => {
-    return (data = {}) => {
-        return new Promise(async (resolve, reject) => {
+	return (data = {}) => {
+		return new Promise(async (resolve, reject) => {
 
-            const dataViews = {
-                user: {},
-                locals: {},
-            };
+			const { locals } = data;
 
-            const [, news] = await Model.news.get({orderBy: 'created DESC'});
+			const dataViews = {
+				user: {},
+				locals: {},
+			};
 
-            dataViews.news = news;
+			const [, news] = await Model.news.get({ orderBy: 'created DESC' });
 
-            Object.assign(dataViews.user, data.locals.user);
-            Object.assign(dataViews.locals, data.locals);
+			const countOnPage = 10;
+			const curPage = locals.reqQuery.page || 1;
+			const pagOffset = (curPage - 1) * countOnPage;
 
-            const templatePath = path.join(__dirname, 'template.ejs');
-            const template = app.render(templatePath, dataViews, (err, str) => {
-                if (err) return resolve([err, err.toString()]);
+			if (pagOffset < 0) {
+				pagOffset = 0;
+			}
+
+			let countPages = news.length / countOnPage;
+
+			if (Number.isInteger(countPages) === false) {
+				countPages = parseInt(countPages + 1)
+			}
+
+			let resArray = news.slice(pagOffset, pagOffset + countOnPage);
+
+			let pagLeft = curPage - 3;
+			let pagRight = +curPage + 3;
+
+			if (pagLeft < 1) {
+				pagRight += +Math.abs(pagLeft);
+				pagLeft = 1;
+			}
+
+			if (pagRight > countPages) {
+				pagRight = countPages + 1;
+			}
+
+			const pagination = {
+				left: pagLeft,
+				right: pagRight,
+				curPage,
+				countPages
+			}
+
+			dataViews.news = resArray;
+			dataViews.pagination = pagination;
+
+			Object.assign(dataViews.user, data.locals.user);
+			Object.assign(dataViews.locals, data.locals);
+
+			const templatePath = path.join(__dirname, 'template.ejs');
+			const template = app.render(templatePath, dataViews, (err, str) => {
+				if (err) return resolve([err, err.toString()]);
 
 
-                return resolve([err, str]);
-            });
-        })
-    }
+				return resolve([err, str]);
+			});
+		})
+	}
 }
