ff558a45aece446ce05490b367d3ff092d5e186e
Zuarel <zuarel@icloud.com>
2018-06-14 21:32:54 +0500

    api/reviews.js change 'add' method

--- app/api/reviews.js
+++ app/api/reviews.js
@@ -2,10 +2,10 @@ exports.add = (req, res, next) => {
 	if (!!req.session.user.id === false) return res.send('Добавление отзывов доступно только авторизованным пользователям');
 
 	const Model = req.app.Model;
-	const { text } = req.body;
+	const { text, targetType, targetId } = req.body;
 	const creator = req.session.user.id;
 
-	return Model.reviews.add({ text, creator }).then(([error, results]) => {
+	return Model.reviews.add({ text, creator, targetType, targetId }).then(([error, results]) => {
 		if (error) return { status: 'bad', message: error.message, error };
 
 		return { status: 'ok' };
