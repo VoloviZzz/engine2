4547c356fb82370fce46db6f81e9d5e07e22a4ee
Zuarel <zuarel@icloud.com>
2018-06-14 21:37:43 +0500

    add pagination, add categories comps/reviews-list

--- app/components/reviews-list/index.js
+++ app/components/reviews-list/index.js
@@ -1,17 +1,47 @@
 const path = require('path');
+const Pagination = require('../../libs/pagination');
 
 module.exports = (app) => {
 	const Model = app.Model;
+
 	return async ({ locals, session, dataViews = {} }) => {
 		// logic...
 
-		const [, reviews] = await Model.reviews.get({ targetType: '', });
-		const [, reviewsNotPublished] = await Model.reviews.get({public: '0'});
+		const { fragment, dynamicRouteNumber: agentId } = locals;
+
+		// пагинация
+		const reviewsCountParams = {
+			targetType: fragment.settings.targetType,
+			targetId: agentId,
+			or: { client_id: session.user.id }
+		};
+
+		var [error, [{ reviews_count: countReviews }]] = await Model.reviews.getCount(reviewsCountParams);
+		const pagination = new Pagination({ countOnPage: 10, allCountPosts: countReviews, currentPage: locals.reqQuery.reviewsPage, pageUrlQuery: locals.reqQuery });
+		// ----------
+
+		const reviewsGet = {
+			public: '1',
+			targetType: fragment.settings.targetType,
+			targetId: agentId,
+			or: {
+				client_id: session.user.id
+			},
+			limit: `${pagination.options.offset}, ${pagination.options.countOnPage}`,
+		};
+
+		var [error, reviews] = await Model.reviews.get(reviewsGet);
+		var [, reviewsNotPublished] = await Model.reviews.get({ public: '0', targetType: fragment.settings.targetType });
+
+		var [error, reviewsCats] = await Model.reviews.getTargets();
 
 		dataViews.reviews = reviews;
 		dataViews.reviewsNotPublished = reviewsNotPublished;
-		dataViews.session = session;
 		dataViews.user = session.user;
+		dataViews.reviewsCats = reviewsCats;
+		dataViews.fragment = fragment;
+		dataViews.agentId = agentId;
+		dataViews.pagination = pagination;
 
 		return new Promise((resolve, reject) => {
 			const template = app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
