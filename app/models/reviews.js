cc68b222af86c855601585d3314966ded5d375b1
Zuarel <zuarel@icloud.com>
2018-06-14 21:39:07 +0500

    add minor code

--- app/models/reviews.js
+++ app/models/reviews.js
@@ -2,16 +2,21 @@ const db = require('../libs/db');
 
 exports.add = (arg = {}) => {
 
-	const { creator, text } = arg;
+	const { creator, text, targetType } = arg;
 
 	if (!!creator === false) return Promise.resolve([new Error('Отсутствует параметр creator')]);
 	if (!!text === false) return Promise.resolve([new Error('Отсутствует параметр text')]);
+	if (!!targetType === false) return Promise.resolve([new Error('Отсутствует параметр targetType')]);
+
+	const targetId = 'targetId' in arg ? `, target_id = '${arg.targetId}'` : '';
 
 	const q = `
 		INSERT INTO reviews
 		SET
 			client_id = ${creator},
-			text = '${text}'
+			text = '${text}',
+			target_type = '${targetType}'
+			${targetId}
 	`;
 
 	return db.insertQuery(q);
@@ -20,10 +25,13 @@ exports.add = (arg = {}) => {
 exports.get = (arg = {}) => {
 
 	let { id, public, client_id, targetType, targetId, limit } = arg;
+	const { or = {} } = arg;
+
+	or.client_id = 'client_id' in or ? `OR client_id = '${or.client_id}'` : '';
 
 	id = !!id === true ? `AND r.id = '${id}'` : ``;
 	client_id = !!client_id === true ? `AND r.client_id = '${client_id}'` : ``;
-	public = typeof public !== "undefined" ? `AND r.published = '${public}'` : ``;
+	public = typeof public !== "undefined" ? `AND (r.published = '${public}' ${or.client_id})` : ``;
 	targetType = typeof targetType !== 'undefined' ? `AND r.target_type = '${targetType}'` : ``;
 	targetId = !!targetId === true ? `AND r.target_id = '${targetId}'` : ``;
 	limit = !!limit === true ? `LIMIT ${limit}` : ``;
@@ -35,14 +43,35 @@ exports.get = (arg = {}) => {
 			LEFT JOIN clients c ON r.client_id = c.id
 		WHERE r.id > 0
 			${id}
-			${public}
 			${targetType}
 			${targetId}
+			${public}
 		ORDER BY r.created DESC
 		${limit}
 	`;
 
 	return db.execQuery(q);
+}
+
+exports.getCount = function (args = {}) {
+	const { or = {} } = args;
+	let { targetType, targetId } = args;
+
+	or.client_id = 'client_id' in or ? `OR r.client_id = '${or.client_id}'` : '';
+
+	targetType = typeof targetType !== 'undefined' ? `AND r.target_type = '${targetType}'` : ``;
+	targetId = !!targetId === true ? `AND r.target_id = '${targetId}'` : ``;
+
+	return db.execQuery(`
+		SELECT COUNT(id) as reviews_count 
+		FROM reviews r 
+		WHERE (r.published = '1' ${or.client_id}) 
+			${targetType} 
+			${targetId}`);
+}
+
+exports.getTargets = (args = {}) => {
+	return db.execQuery(`SELECT target_type as title FROM reviews GROUP BY target_type`);
 }
 
 exports.upd = (arg = {}) => {
