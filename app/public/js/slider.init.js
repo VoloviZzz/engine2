777703d693a49b1098923229f775f7be8efd43d4
Zuarel <zuarel@icloud.com>
2018-06-19 17:24:47 +0500

    slider fix

--- app/public/js/slider.init.js
+++ app/public/js/slider.init.js
@@ -1,86 +1,67 @@
 $(document).ready(function () {
 
-	var sliderItemHeight = 0,
-		carouselItemHeight = 0;
-	var carouselItemsCount = 0,
-		carouselItemWidth = 0;
-	var windowWidth = 0;
-
-	windowWidth = $(window).width();
-
-	carouselItemsCount = 5.5;
-
-	if (windowWidth > 1600) {
-		carouselItemsCount = 5.5;
-	}
-
-	carouselItemWidth = $('#carousel').width() / carouselItemsCount;
-
-	$('.carousel-link__container').find('a').on('click', function (e) {
-		e.stopPropagation();
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: false,
 	});
 
-	// $('#carousel li').css({ width: carouselItemWidth });
+	$('.slider-nav').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		focusOnSelect: true,
+		responsive: [
+			{
+				breakpoint: 1150,
+				settings: {
+					slidesToShow: 3,
+					infinite: true,
+					dots: false,
+				}
+			},
+			{
+				breakpoint: 1300,
+				settings: {
+					slidesToShow: 4,
+					infinite: true,
+					dots: false,
+				}
+			}
+		]
+	}).on("mousewheel", function (event) {
+		event.preventDefault();
+		if (event.deltaX > 0 || event.deltaY < 0) {
+			$(this).slick("slickNext");
+		} else if (event.deltaX < 0 || event.deltaY > 0) {
+			$(this).slick("slickPrev");
+		}
+	})
 
-	var a = $('#carousel').flexslider({
-		animation: "slide",
-		controlNav: false,
-		slideshow: false,
-		itemWidth: carouselItemWidth,
-		itemMargin: 10,
-		asNavFor: '#slider',
-		mousewheel: true,
-		keyboard: false,
-	});
-
-	$('#slider').flexslider({
-		animation: "slide",
-		controlNav: false,
-		directionNav: false,
-		touch: false,
-		animationLoop: false,
-		slideshow: false,
-		sync: "#carousel",
-	});
-
-	$('#alert-slider').flexslider({
-		animation: "slide",
-		directionNav: true,
-		animationLoop: false,
-		slideshow: false
-	});
+	$('.slider-nav .slick-slide').on('click', function() {
+		var nextSlide = $(this).data('slickIndex');
+		$('.slider-for').slick('slickGoTo', nextSlide);
+	})
 
 	setSlidersSize();
-
-	// костыль для того, чтобы родительский элемент слайдера занимал весь экран;
-	$('.slider-container').parent().css({ flex: '1' });
-
-	function setSlidersSize() {
-
-		sliderItemHeight = ($(window).height() - $('.document-header').outerHeight(true) - $('.document-footer').outerHeight(true)) * 0.6;
-		carouselItemHeight = ($(window).height() - $('.document-header').outerHeight(true) - $('.document-footer').outerHeight(true)) * 0.4 - 16;
-		var alertContainerHeight = void 0;
-
-		$('.slider-item').css({ height: sliderItemHeight });
-		$('.carousel-item').css({ height: carouselItemHeight });
-
-		if ($('.alert-container').length) {
-			var setAlertListHeight = setInterval(function () {
-				if ($('.alert-container').position().top > 0) {
-					alertContainerHeight = sliderItemHeight - $('.alert-container').position().top;
-
-					$('.alert-container').css({ height: alertContainerHeight });
-					clearInterval(setAlertListHeight);
-				}
-			}, 0);
-		}
-	}
 
 	$(window).on('resize', function (e) {
 		setTimeout(function () {
 			setSlidersSize();
 		}, 0);
 	});
+
+	function setSlidersSize() {
+		sliderItemHeight = ($(window).height() - $('.document-header').outerHeight(true) - $('.document-footer').outerHeight(true)) * 0.6;
+		//$('.slider-container').css({ height: sliderItemHeight });
+		carouselItemHeight = ($(window).height() - $('.document-header').outerHeight(true) - $('.document-footer').outerHeight(true)) * 0.4;
+
+		$('.slider-for').css({ height: sliderItemHeight });
+		$('.slider-nav').css({ height: carouselItemHeight });
+	}
+
 
 	$('.js-slide-add').on('click', function (e) {
 		e.stopPropagation();
