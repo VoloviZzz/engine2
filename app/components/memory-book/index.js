4d1b4f2f8dc037e35ba133b11cd3954bd45a3c57
Zuarel <zuarel@icloud.com>
2018-06-25 11:38:40 +0500

    update and refact comp/memory-book

--- app/components/memory-book/index.js
+++ app/components/memory-book/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const request = require('request');
 const api = require('../../memory-book-api');
 
+const Pagination = require('../../libs/pagination');
+
 module.exports = (app) => {
 
 	const Model = app.Model;
@@ -15,24 +17,49 @@ module.exports = (app) => {
 			necrologues: [],
 		};
 
+		const { fragment } = locals;
+		const target = fragment.settings.memoryTarget || 'all';
+
+		const currentPage = locals.reqQuery.page || 1;
+		const part = (currentPage - 1) > 0 ? currentPage - 1 : 0;
+
+		const getMemoryParams = {
+			count: 25,
+			memoryTarget: target,
+			part
+		};
+
+		if (!!session.user.adminMode === false) {
+			getMemoryParams.state = '3';
+		}
+
 		try {
-			[data.deads, data.biographies, data.necrologues] = await Promise.all([
-				api.get('deads2', { state: '3' }),
-				api.get('biographies', { state: '3' }),
-				api.get('necrologues', { state: '3' })
-			])
+			data.deads = await api.get('deads2', getMemoryParams);
 		} catch (e) {
 			console.log('Ошибка api "memory_book":');
 			console.log(e);
 		}
 
+		const pagination = new Pagination({ allCountPosts: data.deads.countDeads, countOnPage: getMemoryParams.count, currentPage: currentPage });
+
+		dataViews.memoryTargetsList = [
+			{ target: 'all', title: 'Всё сразу' },
+			{ target: 'biographies', title: 'биографии' },
+			{ target: 'necrologues', title: 'некрологи' },
+			{ target: 'photos', title: 'фото' }
+		];
+
+
 		data.countDeads = data.deads.countDeads || 0;
 		data.deads = data.deads.deads || [];
 		dataViews.data = data;
+		dataViews.fragment = fragment;
 		dataViews.user = session.user;
+		dataViews.pagination = pagination;
+		dataViews.currentMemoryTarget = target;
 
 		return new Promise((resolve, reject) => {
-			const template = app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
+			app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
 				if (err) return resolve([err, err.toString()]);
 
 				return resolve([err, str]);
