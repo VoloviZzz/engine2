a5dffbb6ce309ba1f3b10336ad262a8df2114ac1
Zuarel <zuarel@icloud.com>
2018-06-26 16:01:32 +0500

    shop slider fix

--- app/components/shop-slider/index.js
+++ app/components/shop-slider/index.js
@@ -3,24 +3,34 @@ const path = require('path');
 module.exports = (app) => {
 	return (data = {}) => {
 		return new Promise(async (resolve, reject) => {
-			const [, goodsCategories] = await app.Model.goodsCategories.get({level: 0, orderBy: 'priority DESC'});
+
+			const getGoodsCategoriesParams = {
+				level: 0,
+				orderBy: 'priority DESC'
+			};
+
+			if (data.session.user.adminMode === false) {
+				getGoodsCategoriesParams.public = '1';
+			}
+
+			const [, goodsCategories] = await app.Model.goodsCategories.get(getGoodsCategoriesParams);
 
 			const slides = goodsCategories || [];
 
-			
+
 			const dataViews = {
 				slides,
 				user: {},
 				locals: {},
 			};
-			
+
 			Object.assign(dataViews.user, data.locals.user);
 			Object.assign(dataViews.locals, data.locals);
-			
+
 			const templatePath = path.join(__dirname, 'template.ejs');
 			const template = app.render(templatePath, dataViews, (err, str) => {
-				if(err) return resolve([err, err.toString()]);
-				
+				if (err) return resolve([err, err.toString()]);
+
 				return resolve([err, str]);
 			});
 		})
