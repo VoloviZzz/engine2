d83fff44412ccdc539a1d7c570884c39ddc360b3
Zuarel <zuarel@icloud.com>
2018-06-29 17:18:29 +0500

    update agents-list

--- app/components/agents-list/index.js
+++ app/components/agents-list/index.js
@@ -9,22 +9,20 @@ module.exports = (app) => {
 
 		var currentTarget = 'Вызов агентов';
 
-		const postsGetParams = {
-			target: currentTarget,
-		};
+		const postsGetParams = {};
 
 		if (session.user.adminMode == false) {
 			postsGetParams.public = '1';
 		}
 
-		var [error, agents] = await Model.posts.get(postsGetParams);
+		var [error, agents] = await Model.agents.get(postsGetParams);
 
 		dataViews.agents = agents;
 		dataViews.user = session.user;
 		dataViews.fragment = locals.fragment;
 
 		return new Promise((resolve, reject) => {
-			const template = app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
+			app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
 				if (err) return resolve([err, err.toString()]);
 
 				return resolve([err, str]);
