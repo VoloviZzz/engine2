9a8679575382fce3a2f19a90058745f5004fee29
Zuarel <zuarel@icloud.com>
2018-07-05 11:42:20 +0500

    update post-view: add similar posts and patch

--- app/components/post-view/index.js
+++ app/components/post-view/index.js
@@ -17,8 +17,34 @@ module.exports = (app) => {
 				return Promise.resolve([, 'Публикая не найдена']);
 			}
 
+			var similarPosts = null;
+
 			var [error, postCategories] = await app.db.execQuery(`SELECT * FROM post_categories WHERE target_id = '${post.target}'`);
 
+			if (post.similar_posts_id) {
+				var [error, similarPosts] = await app.db.execQuery(`SELECT * FROM posts WHERE id IN (${post.similar_posts_id})`);
+			}
+			else {
+				const countSimilarPosts = 5;
+				var [error, similarPosts] = await app.db.execQuery(`SELECT * FROM posts WHERE cat = '${post.cat}' AND target = '${post.target}' AND id <> '${post.id}' ORDER BY RAND() LIMIT ${countSimilarPosts}`);
+
+				var similarIds = similarPosts.reduce((prev, current) => {
+					prev.push(current.id);
+					return prev;
+				}, []).join(',');
+
+				await db.execQuery(`UPDATE posts SET similar_posts_id = '${similarIds}' WHERE id = '${post.id}'`);
+
+				post.similar_posts_id = similarIds;
+			}
+
+
+			if (error) {
+				console.log(error);
+				return Promise.resolve(["Что-то пошло не так"]);
+			}
+
+			dataViews.similarPosts = similarPosts;
 			dataViews.post = post;
 			dataViews.postCategories = postCategories;
 			locals.route.title = post.title;
