34c814a5e836ff6d01be041265ea157d773f261e
Zuarel <bubl174rus@gmail.com>
2018-08-20 17:53:48 +0500

    add unloading

--- app/unloading/functions/send_sold_items.js
+++ app/unloading/functions/send_sold_items.js
@@ -1,9 +1,41 @@
 const sendPost = require('./send_post');
+const db = require('../../libs/db');
 
 module.exports = async (data) => {
-	// если пустой массив с позициями, ничего не делать
-	if (data.poses.length < 1) return false;
-	var sendItemsResult = await sendPost(`http://25.27.135.50:80/shop_api/`, { data: JSON.stringify(data), path: 'sales_export', func: 'sales_export' });
+	try {
+		// console.log(data);
+		// если пустой массив с позициями, ничего не делать
+		if (data.poses.length < 1) return false;
 
-	console.log(sendItemsResult);
+		const { poses, client_info } = data;
+
+		const groupPosesByCrmId = {};
+
+		for (const position of poses) {
+			const { connect_id } = position;
+
+			if (!!connect_id === false) continue;
+
+			if (!!groupPosesByCrmId[connect_id] === false) {
+				groupPosesByCrmId[connect_id] = [];
+			}
+
+			groupPosesByCrmId[connect_id].push(position);
+		}
+
+		for (const connect_id of Object.keys(groupPosesByCrmId)) {
+			var [error, connectedCrmArray] = await db.execQuery(`SELECT * FROM connected_crm WHERE id = '${connect_id}'`);
+			if (connectedCrmArray.length != 1) continue;
+
+			const connectedCrm = connectedCrmArray[0];
+			const { host, port, token } = connectedCrm;
+			const crmPoses = groupPosesByCrmId[connect_id];
+
+			const sendItemsResult = await sendPost(`http://${host}:${port}/shop_api/`, { data: JSON.stringify({ client_info, poses: crmPoses }), path: 'sales_export', func: 'sales_export' });
+
+			console.log('------>', sendItemsResult);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
--- app/unloading/index.js
+++ app/unloading/index.js
@@ -1,193 +1,31 @@
 const express = require('express');
-const router = express.Router();
 
 const app = express();
-const db = require(`../libs/db`);
-const Model = require('../models');
-
-const formidable = require('formidable');
-const request = require(`request`);
-const fs = require('fs');
 const path = require('path');
-
-const sendPost = require('./functions/send_post');
-
-const saveImageByUrl = function saveImageByUrl(url, file) {
-	return new Promise((resolve, reject) => {
-		const stream = request(url).pipe(file);
-
-		stream.on('finish', () => resolve(true));
-	})
-}
 
 app.set('views', path.join(__dirname, 'views'));
 
-// request.post(`http://es/shop_api/`, { form: { func: 'ping', path: 'ping' } }, (error, response, body) => {	
-// 	try {
-// 		body = JSON.parse(body);
-// 		console.log(body);
-// 	} catch (error) {
-// 		console.error("Ошибка:");
-// 		console.error(error);
-// 	}
-// })
+app.use(require('./middlewares/parseReqBody'));
+app.use(require('./middlewares/requestLogTime'));
 
-app.use((req, res, next) => {
-	console.log(new Date().toLocaleTimeString(), `Пришёл запрос на адрес: ${req.url}`);
-	next();
-});
+app.get(`/`, require('./routes/home'));
 
-app.get(`/`, (req, res, next) => {
-	console.log(res);
-	res.render(`info`);
-});
+app.post(`/ping`, require('./routes/ping'));
 
-app.post('/addConnection', async function addConnection(req, res, next) {
-	try {
-		const { host, port, siteName: title } = req.body;
-		const { status: serverStatus } = await sendPost(`http://${host}:${port}/shop_api/`, { path: `connect-crm`, func: `ping` });
+app.post('/addConnection', require(`./routes/addConnection`));
 
-		if (serverStatus !== 'ok') {
-			return res.json({ status: 'bad', message: 'На данный момент сервер недоступен' });
-		}
+app.post(`/addPositions`, require(`./routes/positions`).add);
+app.post(`/getPositions`, require(`./routes/positions`).get);
+app.post(`/updatePositions`, require(`./routes/positions`).update);
+app.post(`/deletePositions`, require('./routes/positions').delete);
+app.get(`/getCategories`, require('./routes/getCategories'));
+app.post(`/getCategories`, require('./routes/getCategories'));
 
-		const connectResult = await sendPost(`http://${host}:${port}/shop_api/`, { path: `connect-crm`, func: `connect-crm`, data: JSON.stringify({ host: '25.47.255.136', port: '3000', title }) });
+app.post(`/deletePhotos`, require('./routes/photos').delete);
+app.post(`/uploadPhotos`, require('./routes/photos').upload);
 
-		if (connectResult.status !== 'ok') {
-			return res.json({ status: 'bad', message: 'Что-то пошло не так' });
-		}
+app.post('/connectionResponse', require('./routes/connectionResponse'));
 
-		let { data: crm_id } = connectResult;
-
-		await db.execQuery(`INSERT INTO connected_crm SET title = '${title}', host = '${host}', port = '${port}', crm_id = '${crm_id}'`);
-
-		res.json({ status: 'ok' });
-	} catch (error) {
-		console.error(`Произошла ошибка`);
-		console.error(error);
-
-		res.json({ status: 'bad', ...error });
-	}
-});
-
-app.post(`/updatePositions`, (req, res, next) => {
-
-	var form = new formidable.IncomingForm();
-
-	form.parse(req, async function (err, fields, files) {
-		try {
-			const { id, target, value } = JSON.parse(fields.data);
-			const targetsWhiteList = ['price', 'count'];
-
-			const targetIsValid = targetsWhiteList.includes(target);
-
-			if (targetIsValid === false) {
-				return res.json({ status: 'bad', message: 'Недопустимый параметр target' });
-			}
-
-			if (!!value === false && value !== 0 && value !== '') {
-				return res.json({ status: 'bad', message: 'Недопустимый параметр value' });
-			}
-
-			var [error] = await Model.goodsPositions.upd({ id, target, value });
-
-			if (error) {
-				return res.json({ status: 'bad', message: `Ошибка во время выполнения запроса: ${error.message}`, error });
-			}
-
-			return res.json({ status: 'ok' });
-		} catch (error) {
-			res.json({ status: 'bad' });
-		}
-	});
-
-	form.on('error', (error) => console.log(`Ошибка при получение данных с программы. ${error.message}`));
-});
-
-/**
- * Получает от программы ответ на запрос сопряжения
- */
-app.post('/connectionResponse', async function connectionResponse(req, res, next) {
-	var form = new formidable.IncomingForm();
-
-	form.parse(req, async function (err, fields, files) {
-		try {
-			const { id, token } = JSON.parse(fields.data);
-
-			console.log(id, token);
-
-			var [error] = await db.execQuery(`UPDATE connected_crm SET token = '${token}' WHERE crm_id = '${id}'`);
-
-			console.log(error);
-
-			return res.json({ status: 'ok' });
-		} catch (error) {
-			res.json({ status: 'bad' });
-		}
-	});
-
-	form.on('error', (error) => console.log(`Ошибка при получение данных с программы. ${error.message}`));
-});
-
-app.post(`/ping`, (req, res, next) => {
-	res.json({ status: 'ok' });
-});
-
-app.post(`/getCategories`, (req, res, next) => {
-	db.execQuery(`SELECT * FROM goods_cats`).then(([error, rows]) => {
-		if (error) return next(error);
-
-		res.json({ status: 'ok', data: rows });
-	})
-});
-
-
-app.post(`/addPositions`, function addPositions(req, res, next) {
-
-	var form = new formidable.IncomingForm();
-
-	form.parse(req, async function (err, fields, files) {
-		try {
-
-			const positions = JSON.parse(fields.data);
-			const returnArray = [];
-
-			for (const position of positions) {
-				const { name: title, cat_data: cat_id, description, pos_id, mod_id, count, ph_data: photosStringify, price, sa_id: crm_id } = position;
-				const photosArray = photosStringify ? JSON.parse(photosStringify) : [];
-
-				const savePhotoPromises = [];
-
-				for (const photo of photosArray) {
-					const photoParse = path.parse(photo.origin);
-					const file = fs.createWriteStream(`app/public/uploads/${photoParse.name}${photoParse.ext}`);
-					const fileUrl = `http://es${photo.origin}`;
-					const savePhotoPromise = saveImageByUrl(fileUrl, file);
-
-					savePhotoPromises.push(savePhotoPromise);
-				}
-
-				const savePhotoResult = await Promise.all(savePhotoPromises);
-
-				const [error, id] = await Model.goodsPositions.add({ title, cat_id, description, count, price, pos_id, mod_id, crm_id });
-
-				returnArray.push({ id, pos_id, mod_id });
-			}
-
-			console.log(`Запрос успешно завершён`);
-			return res.json({ status: 'ok', data: returnArray });
-		} catch (error) {
-			console.log(`Произошла ошибка`);
-			console.log(error);
-			return res.json({ status: 'bad', ...error, error });
-		}
-	});
-
-	form.on('error', (error) => console.log(`Ошибка при получение данных с программы. ${error.message}`));
-})
-
-app.use((error, req, res, next) => {
-	res.json({ message: error.message, error, status: 'bad' });
-})
+app.use(require('./middlewares/errorHandler'));
 
 module.exports = app;
