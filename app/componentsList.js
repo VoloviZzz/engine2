0b90b62d03709c4588e4300f8c7adb6345b99c75
Zuarel <bubl174rus@gmail.com>
2018-08-27 20:44:26 +0500

    minor update componentsList.js

--- app/componentsList.js
+++ app/componentsList.js
@@ -1,28 +1,27 @@
 module.exports = async app => {
     const db = require('./libs/db');
 
-    return await db.execQuery('SELECT * FROM `components` ORDER BY `components`.`title` ASC').then(([error, rows]) => {
-        if (error) throw new Error(error);
+    var [error, rows] = await db.execQuery('SELECT * FROM `components` ORDER BY `components`.`title` ASC');
+    if (error) throw new Error(error);
 
-        app.componentsList = rows;
-        app.componentsObj = {};
-        componentsObj = app.componentsObj;
+    app.componentsList = rows;
+    app.componentsObj = {};
+    
+    componentsObj = app.componentsObj;
 
+   generalComponents = [];
 
-        generalComponents = [];
+    rows.forEach(component => {
+        const { block_id } = component;
 
-        rows.forEach(component => {
-            const { block_id } = component;
+        if (!!block_id === false) return generalComponents.push(component);
+        if (!!componentsObj[block_id] === false) componentsObj[block_id] = [];
+        componentsObj[block_id].push(component);
+    })
 
-            if(!!block_id === false) return generalComponents.push(component);
-            if(!!componentsObj[block_id] === false) componentsObj[block_id] = [];
-            componentsObj[block_id].push(component);
-        })
+    Object.keys(componentsObj).forEach(block_id => {
+        componentsObj[block_id] = componentsObj[block_id].concat(generalComponents);
+    })
 
-        Object.keys(componentsObj).forEach(block_id => {
-            componentsObj[block_id] = componentsObj[block_id].concat(generalComponents);
-        })
-
-        return rows;
-    });
+    return rows;
 }
