6b0b56f99afad70f5fb1840b73379a145f888e9b
Zuarel <bubl174rus@gmail.com>
2018-09-05 21:16:17 +0500

    update /libs/db.js: fix mysql queries

--- app/libs/db.js
+++ app/libs/db.js
@@ -24,32 +24,35 @@ exports.get = function () {
 	return state.pool
 }
 
-exports.execQuery = function (queryStr, data = {}) {
+exports.execQuery = function (queryStr, data = false) {
 	return new Promise((resolve, reject) => {
-		exports.get().query(queryStr, data, (err, rows) => {
-			if (err) return resolve([err, null, queryStr]);
-			return resolve([err, rows, queryStr]);
-		})
+		if (data === false) {
+			exports.get().query(queryStr, (err, rows) => {
+				if (err) return resolve([err, null, queryStr]);
+				return resolve([err, rows, queryStr]);
+			})
+		} else {
+			exports.get().query(queryStr, data, (err, rows) => {
+				if (err) return resolve([err, null, queryStr]);
+				return resolve([err, rows, queryStr]);
+			})
+		}
 	})
 }
 
-exports.insertQuery = function (queryStr, data = {}) {
+exports.insertQuery = function (queryStr, data = false) {
 	return new Promise((resolve, reject) => {
-		exports.get().query(queryStr, data, (err, rows) => {
-			if (err) return resolve([err, null]);
-			return resolve([err, rows.insertId]);
-		})
-	})
-}
 
-exports.updateQuery = ({ table = '', data = {}, fields = {} }) => {
-	let fieldsStr = Object.keys(fields).map(fieldName => `${fieldName} = '${fields[fieldName]}'`).join(', ');
-	let queryStr = `UPDATE \`${table}\` SET ${fieldsStr} WHERE id = ${data.id}`;
-
-	return new Promise((resolve, reject) => {
-		exports.get().query(queryStr, {}, (err, rows) => {
-			if (err) return resolve([err, null]);
-			return resolve([err, rows]);
-		})
+		if (data === false) {
+			exports.get().query(queryStr, (err, rows) => {
+				if (err) return resolve([err, null]);
+				return resolve([err, rows.insertId]);
+			})
+		} else {
+			exports.get().query(queryStr, data, (err, rows) => {
+				if (err) return resolve([err, null]);
+				return resolve([err, rows.insertId]);
+			})
+		}
 	})
 }
