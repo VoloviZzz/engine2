cf2b8172c0508e90e97797fe495f8004c6301ff9
operan38 <gorbynov2000@yandex.ru>
2018-09-13 22:22:41 +0500

    small fix template

--- app/public/js/application.js
+++ app/public/js/application.js
@@ -445,7 +445,7 @@ RoutesList.prototype.updRoute = function (data) {
 
 RoutesList.prototype.showEditForm = function (elem) {
 	var $this = $(elem);
-	var $routeItem = $this.parent('td');
+	var $routeItem = $('td[data-route-id="'+$this.attr('data-route-id')+'"]');
 
 	$routeItem.toggleClass('js-edit-form--show');
 };
