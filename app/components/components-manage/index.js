9e8ed5c5781ea617a5e04ebd02b94d1da46eda40
Zuarel <bubl174rus@gmail.com>
2018-09-19 16:53:26 +0500

    change add components form

--- app/components/components-manage/index.js
+++ app/components/components-manage/index.js
@@ -1,19 +1,23 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = (app) => {
 
 	const Model = app.Model;
-	
+
 	return async ({ locals, session, dataViews = {} }) => {
 		// logic...
 
-		const [, components] = await Model.components.get();
-		const [, componentBlocks] = await Model.components.getComponentBlocks();
+		var [error, components] = await Model.components.get();
+		var [error, componentBlocks] = await Model.components.getComponentBlocks();
 
+		const controllersList = fs.readdirSync(`${AppRoot}/components`);
 
 		dataViews.componentsList = components;
 		dataViews.componentBlocks = componentBlocks;
-		
+		dataViews.user = session.user;
+		dataViews.controllersList = controllersList;
+
 		return new Promise((resolve, reject) => {
 			const template = app.render(path.join(__dirname, 'template.ejs'), dataViews, (err, str) => {
 				if (err) return resolve([err, err.toString()]);
