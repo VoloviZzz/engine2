e46bf189b42cd4f0038e94c75fb180cc345128b2
Zuarel <bubl174rus@gmail.com>
2018-10-17 15:17:38 +0500

    fix regexp pattern

--- app/api/routes-aliases.js
+++ app/api/routes-aliases.js
@@ -3,7 +3,7 @@ const Model = require('../models');
 exports.add = async (req, res, next) => {
 	try {
 
-		if (/^\/[0-9A-Za-zА-Яа-я]+$/.test(req.body.alias) === false) return { message: 'Неверный формат алиаса.' };
+		if (/^\/[0-9A-Za-zА-Яа-я-]+$/.test(req.body.alias) === false) return { message: 'Неверный формат алиаса.' };
 
 		var [error] = await Model.aliases.add(req.body);
 		if (error) throw new Error(error);
